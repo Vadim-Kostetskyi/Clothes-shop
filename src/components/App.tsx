0a3fc3e65a5f7ccba4163d15769bf7b4f754fbd4
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-10-27 01:36:29 +0300

    create footer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
+
+import './App.css'
 import HomePage from './HomePage';
+
+import { Footer } from './Footer/Footer';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Footer />} />
     </Routes>
 
   );
