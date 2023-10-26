0625a66983996af5cbc0af617960c6a0c99f953e
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-10-27 01:38:15 +0300

    fix app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,12 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import HomePage from './HomePage';
 
-import { Footer } from './Footer/Footer';
-
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Footer />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
 
   );
