c701536f9255f0daaf768bd8db7bc0372d6f56c1
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-10-29 00:27:39 +0300

    placing svg in a separate file

--- src/components/Footer/FooterList.tsx
+++ src/components/Footer/FooterList.tsx
@@ -1,12 +1,16 @@
+import { MenuContext } from './MenuList';
 import style from './Footer.module.css'
 
 const FooterList = ({device, listVisible, openList}:{device: string, listVisible?: boolean[], openList?: Function}): JSX.Element => {
-    
+  const { m1, m2, m3 } = MenuContext();
+  m1.map(el => console.log(el.id)
+  )
+  
   return (
     <>
       <div className={device}>
         <div className={style.titleBox}>
-          <h1 className={style.title}>Help</h1>
+          <h3 className={style.title}>Help</h3>
           {(listVisible && !listVisible[0]) ? (<button className={style.listButton} onClick={() => openList ? openList(0) : ''}>
           <svg className={style.listImg} viewBox="0 0 35 35">
             <path d="M22.919 11.065h-10.011v-10.011c0-0.509-0.413-0.922-0.922-0.922s-0.922 0.413-0.922 0.922v10.011h-10.011c-0.509 0-0.922 0.413-0.922 0.922s0.413 0.922 0.922 0.922h10.011v10.011c0 0.509 0.413 0.922 0.922 0.922s0.922-0.413 0.922-0.922v-10.011h10.011c0.509 0 0.922-0.413 0.922-0.922s-0.413-0.922-0.922-0.922z"></path>
@@ -18,18 +22,16 @@ const FooterList = ({device, listVisible, openList}:{device: string, listVisible
             </svg>
           </button>) : null}
         </div>
-        <ul className={(listVisible && listVisible[0]) ? style.listOpenHelp : style.list}>
-          <li className={style.item}>Payment</li>
-          <li className={style.item}>Delivery</li>
-          <li className={style.item}>Returns</li>
-          <li className={style.item}>Gift Card</li>
-          <li className={style.item}>Guest purchase</li>
-          <li className={style.item}>Electronic receipt</li>
-        </ul>
+        <nav className={(listVisible && listVisible[0]) ? style.listOpenHelp : style.list}>
+          {m1.map(el => {
+            return (
+            <a href="#" className={style.link} key={el.id}>{el.label}</a>
+          )})}
+        </nav>
       </div>
       <div className={device}>
         <div className={style.titleBox}>
-          <h1 className={style.title}>Terms</h1>
+          <h3 className={style.title}>Terms</h3>
             {(listVisible && !listVisible[1]) ? (<button className={style.listButton} onClick={() => openList ? openList(1) : ''}>
           <svg className={style.listImg} viewBox="0 0 35 35">
             <path d="M22.919 11.065h-10.011v-10.011c0-0.509-0.413-0.922-0.922-0.922s-0.922 0.413-0.922 0.922v10.011h-10.011c-0.509 0-0.922 0.413-0.922 0.922s0.413 0.922 0.922 0.922h10.011v10.011c0 0.509 0.413 0.922 0.922 0.922s0.922-0.413 0.922-0.922v-10.011h10.011c0.509 0 0.922-0.413 0.922-0.922s-0.413-0.922-0.922-0.922z"></path>
@@ -41,16 +43,16 @@ const FooterList = ({device, listVisible, openList}:{device: string, listVisible
             </svg>
           </button>) : null}
         </div>
-        <ul className={(listVisible && listVisible[1]) ? style.listOpen : style.list}>
-          <li className={style.item}>Privacy policy</li>
-          <li className={style.item}>Cookie settings</li>
-          <li className={style.item}>Cookies policy</li>
-          <li className={style.item}>Site Map</li>
-        </ul>
+        <nav className={(listVisible && listVisible[1]) ? style.listOpen : style.list}>
+          {m2.map(el => {
+            return (
+            <a href="#" className={style.link} key={el.id}>{el.label}</a>
+          )})}
+        </nav>
       </div>
       <div className={device}>
         <div className={style.titleBox}>
-          <h1 className={style.title}>We are NOVA</h1>
+          <h3 className={style.title}>We are NOVA</h3>
           {(listVisible && !listVisible[2]) ? (<button className={style.listButton} onClick={() => openList ? openList(2) : ''}>
           <svg className={style.listImg} viewBox="0 0 35 35">
             <path d="M22.919 11.065h-10.011v-10.011c0-0.509-0.413-0.922-0.922-0.922s-0.922 0.413-0.922 0.922v10.011h-10.011c-0.509 0-0.922 0.413-0.922 0.922s0.413 0.922 0.922 0.922h10.011v10.011c0 0.509 0.413 0.922 0.922 0.922s0.922-0.413 0.922-0.922v-10.011h10.011c0.509 0 0.922-0.413 0.922-0.922s-0.413-0.922-0.922-0.922z"></path>
@@ -62,12 +64,12 @@ const FooterList = ({device, listVisible, openList}:{device: string, listVisible
             </svg>
           </button>) : null}
         </div>
-        <ul className={(listVisible && listVisible[2]) ? style.listOpen : style.list} >
-          <li className={style.item}>About Nova</li>
-          <li className={style.item}>Join Life</li>
-          <li className={style.item}>Work with us</li>
-          <li className={style.item}>Press</li>
-        </ul> 
+        <nav className={(listVisible && listVisible[2]) ? style.listOpen : style.list} >
+          {m3.map(el => {
+            return (
+            <a href="#" className={style.link} key={el.id}>{el.label}</a>
+          )})}
+        </nav> 
       </div>
     </>
   );
