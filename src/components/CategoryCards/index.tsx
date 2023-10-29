1fc61e163d4a34155044b715a85f8c7b48ac2bf1
Tetiana Nosenko <tanya2016english@gmail.com>
2023-10-29 17:05:02 -0400

    make homepage responsive

--- src/components/CategoryCards/index.tsx
+++ src/components/CategoryCards/index.tsx
@@ -1,6 +1,9 @@
 import WomenCategoryImage from '../../assets/images/women-category-image.png';
 import KidsCategoryImage from '../../assets/images/kids-category-image.png';
 import MenCategoryImage from '../../assets/images/men-category-image.png';
+import WomenCategoryImageSmall from '../../assets/images/women-category-image-small.png';
+import KidsCategoryImageSmall from '../../assets/images/kids-category-image-small.png';
+import MenCategoryImageSmall from '../../assets/images/men-category-image-small.png';
 import CategoryCard from 'components/CategoryCard';
 import { CategoryCardProps } from 'components/CategoryCard';
 import styles from './index.module.scss';
@@ -9,17 +12,20 @@ const categories: CategoryCardProps[] = [
   {
     id: 0,
     image: WomenCategoryImage,
+    imageSmall: WomenCategoryImageSmall,
     category: 'Women',
   },
  
   {
     id: 1,
     image: MenCategoryImage,
+    imageSmall: MenCategoryImageSmall,
     category: 'Men',
   },
   {
     id: 2,
     image: KidsCategoryImage,
+    imageSmall: KidsCategoryImageSmall,
     category: 'Kids',
   },
 ]
@@ -27,7 +33,7 @@ const categories: CategoryCardProps[] = [
 const CategoryCards = () => {
   return (
     <div className={styles.cardsWrapper}>
-      {categories.map(({id, image, category}) => <CategoryCard id={id} key={id} image={image} category={category}/>)}
+      {categories.map(({ id, image,imageSmall, category }) => <CategoryCard id={id} key={id} image={image} imageSmall={imageSmall} category={category}/>)}
     </div>
   )
 }
