d43f07957fea183078619451893e1f3d1755979a
Vadim <113288447+Vadim-Kostetskyi@users.noreply.github.com>
2023-10-30 20:53:16 +0200

    Footer revision (#9)
    * create footer
    
    * fix app
    
    * add mobile version
    
    * fix acardion animation
    
    * add empty, chenge export cinst on export default
    
    * placing svg in a separate file
    
    * replacing css with scss
    
    * code fixing
    
    * corrected the names
    
    * del return
    
    * marge
    
    * del empty
    
    * add href to links, add destructuring
    
    ---------
    
    Co-authored-by: storm2107 <113288447+storm2107@users.noreply.github.com>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,43 +1,55 @@
 import { useState } from 'react'
-import { Instagram, Facebook, Twitter, Pinterest } from 'assets/SVG/Footer/SocialSvg'
-import { Earth, Phone } from 'assets/SVG/Footer/InfoSvg'
 import FooterList from './FooterList'
-import style from './Footer.module.scss'
+import InstagramImg from 'assets/SVG/Footer/Instagram'
+import FacebookImg from 'assets/SVG/Footer/Facebook'
+import TwitterImg from 'assets/SVG/Footer/Twitter'
+import PinterestImg from 'assets/SVG/Footer/Pinterest'
+import EarthImg from 'assets/SVG/Footer/Earth'
+import PhoneImg from 'assets/SVG/Footer/Phone'
+import styles from './Footer.module.scss'
 
 const Footer = (): JSX.Element => { 
-    const [listVisible, setListVisible] = useState([false, false, false]);
+  const [listVisible, setListVisible] = useState([false, false, false]);
     
   const openList = (number: number) => {
-        setListVisible((prev) => prev.map((value, index) => (index === number ? !value : value)))
+    setListVisible((prev) => prev.map((value, index) => (index === number ? !value : value)))
     }
     
     return ( 
-        <footer className={style.footer}>
-            <div className={style.shadowBorder}></div>
-            <div className={style.box}>
-                <FooterList device={style.columnDesktop} />
-                <div className={style.info}>
-                    <div className={style.social}> 
-                        <Facebook className={style.imgSocial} />
-                        <Instagram className={style.imgSocial} />
-                        <Twitter className={style.imgSocial} />
-                        <Pinterest className={style.imgSocial} />
-                    </div>
-                    <div className={style.infoBox}>
-                        <Earth className={style.img} />
-                        <p className={style.language}>Ukraine | English</p>
-                    </div>
-                    <div className={style.infoBox} style={{marginBottom: 8}}>
-                        <Phone className={style.img} />
-                        <p className={style.number}>Call 900 456 003</p>
-                        <p className={style.active}>Online</p>
-                    </div>
-                    <p className={style.workDays}>From Mondays to Fridays from 09:00 to 19:00</p>
-                <FooterList device={style.columnMobile} listVisible={listVisible} openList={openList} />
-                </div>
+      <footer className={styles.footer}>
+        <div className={styles.shadowBorder}></div>
+        <div className={styles.box}>
+          <FooterList className={styles.columnDesktop} />
+          <div className={styles.info}>
+            <div className={styles.social}> 
+              <button className={styles.socialBtn}>
+                <FacebookImg />
+              </button>
+              <button className={styles.socialBtn}>
+                <InstagramImg />
+              </button>
+              <button className={styles.socialBtn}>
+                <TwitterImg />
+              </button>
+              <button className={styles.socialBtn}>
+                <PinterestImg />
+              </button>
             </div>
-            <p className={style.copyright}>© 2023 by NOVA. All Rights Reserved.</p>
-        </footer>
+            <button className={styles.infoBox}>
+              <EarthImg className={styles.img} />
+              <p className={styles.language}>Ukraine | English</p>
+            </button>
+            <button className={styles.infoBox} style={{marginBottom: 8}}>
+              <PhoneImg className={styles.img} />
+              <p className={styles.number}>Call 900 456 003</p>
+              <p className={styles.active}>Online</p>
+            </button>
+            <p className={styles.workDays}>From Mondays to Fridays from 09:00 to 19:00</p>
+            <FooterList className={styles.columnMobile} listVisible={listVisible} openList={openList} />
+          </div>
+        </div>
+        <p className={styles.copyright}>© 2023 by NOVA. All Rights Reserved.</p>
+      </footer>
     )
 }
 
