9df843fb35ed5aee3af5f68434c63d980fbee8f4
Tetiana Nosenko <tanya2016english@gmail.com>
2023-10-30 21:07:16 -0400

    add swiper

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import Footer from './Footer';
 import HomePage from './HomePage';
 
 function App() {
--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,15 +1,19 @@
 import CategoryCards from "components/CategoryCards";
 import Header from "components/Header";
+import Swiper from "components/Swiper";
 import styles from './index.module.scss';
 
 const HomePage = () => {
   return (
+    <>
     <div className={styles.wrapperHomePage}>
       <Header />
       <div className={styles.wrapperCategoryCards}>
         <CategoryCards />
       </div>
     </div>
+    <Swiper />
+    </>
   )
 }
 
