a7d2d3e08a002b7fefcbd901339487249af6de17
Vadim-Kostetskyi <rasty2308@gmail.com>
2023-11-08 19:03:15 +0200

    using link on logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import MenuLayout from './MenuLayout';
 import TopBar from './TopBar';
 import logo from '../../assets/images/logo.png';
 import styles from './index.module.scss';
 
-// interface HeaderProps {
-//   homePage: boolean
-// }
-
 const Header = (): JSX.Element => {
-
   const location = useLocation();
   const isHomePage = location.pathname === '/';
+
   return (
     <div>
       <div
@@ -22,7 +18,9 @@ const Header = (): JSX.Element => {
         <nav className={isHomePage ? styles.navigation : styles.invisible}>
           <MenuLayout />
         </nav>
-        <img src={logo} className={styles.logo} alt="Logo" /> {/* home page */}
+        <Link to='/'>
+          <img src={logo} className={styles.logo} alt="Logo" />
+        </Link>
         <div className={isHomePage ? styles.userBox : styles.invisible}>
           <TopBar />
         </div>
