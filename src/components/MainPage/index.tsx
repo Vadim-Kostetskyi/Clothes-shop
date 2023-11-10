e829bd0a81b2e352baa02932260ffc1f7704cbb1
Vadim <113288447+Vadim-Kostetskyi@users.noreply.github.com>
2023-11-10 21:16:31 +0200

    fix header on home page, add header and footer to main page (#19)
    * fix header on home page, add header and footer to main page
    
    * fix
    
    * fix

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import CollectionImageSwiper from 'components/CollectionImageSwiper';
 import FormSubscription from 'components/FormSubscription';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
 import styles from './index.module.scss';
 
 const MainPage = () => (
   <>
+    <Header />
     <div className={styles.wrapper}>
       <CollectionImageSwiper />
       <FormSubscription />
+      <Footer />
     </div>
   </>
 );
