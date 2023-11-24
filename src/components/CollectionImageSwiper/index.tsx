d25067b639b32145bbd87dbf3ff60bd920f17873
Tetiana Nosenko <tanya2016english@gmail.com>
2023-11-24 11:20:34 -0500

    fix imports

--- src/components/CollectionImageSwiper/index.tsx
+++ src/components/CollectionImageSwiper/index.tsx
@@ -7,12 +7,17 @@ import Image1 from '../../assets/images/swiper-man-image-1.png';
 import Image2 from '../../assets/images/swiper-man-image-2.png';
 import Image3 from '../../assets/images/swiper-man-image-3.png';
 import Image4 from '../../assets/images/swiper-man-image-4.png';
+import ImageSmall1 from '../../assets/images/swiper-man-image-small-1.png';
+import ImageSmall2 from '../../assets/images/swiper-man-image-small-2.png';
+import ImageSmall3 from '../../assets/images/swiper-man-image-small-3.png';
+import ImageSmall4 from '../../assets/images/swiper-man-image-small-4.png';
 import styles from './index.module.scss';
 import 'swiper/scss/pagination';
 
 export interface SlidesProps {
   id: number;
   image: string;
+  imageSmall: string;
   text: string;
 }
 
@@ -21,21 +26,25 @@ const slides: SlidesProps[] = [
     id: 0,
     text: 'Golden Hour Glam',
     image: Image1,
+    imageSmall: ImageSmall1,
   },
   {
     id: 1,
     text: 'Autumn Eleganse',
     image: Image2,
+    imageSmall: ImageSmall2,
   },
   {
     id: 2,
     text: 'No Limits',
     image: Image3,
+    imageSmall: ImageSmall3,
   },
   {
     id: 3,
     text: 'Symphony of clothes',
     image: Image4,
+    imageSmall: ImageSmall4,
   },
 ];
 
@@ -61,23 +70,34 @@ const CollectionImageSwiper = () => {
           delay: 5000,
         }}
       >
-        {slides.map(({ text, image, id }) => (
+        {slides.map(({ text, image, imageSmall, id }) => (
           <SwiperSlide key={id}>
-            <img src={image} alt={text} className={styles.image} />
-            <div className={styles.test}>
+            <div className={styles.itemsWrapper}>
+              <picture className={styles.imagesWrapper}>
+                <source media="(max-width: 960px)" srcSet={imageSmall} />
+                <img src={image} alt={text} className={styles.image} />
+                <div className={styles.wrapperArrows}>
+                  <button
+                    ref={node => setPrevEl(node)}
+                    className={styles.itemArrows}
+                  >
+                    <ArrowSwiper className={styles.arrow} />
+                  </button>
+                  <button
+                    ref={node => setNextEl(node)}
+                    className={styles.itemArrows}
+                  >
+                    <ArrowSwiper
+                      className={`${styles.arrow} ${styles.arrowNext}`}
+                    />
+                  </button>
+                </div>
+              </picture>
               <p className={styles.text}>{text}</p>
             </div>
           </SwiperSlide>
         ))}
-        <div className={styles.wrapperArrows}>
-          <button ref={node => setPrevEl(node)} className={styles.itemArrows}>
-            <ArrowSwiper className={styles.arrows} />
-          </button>
-          <button ref={node => setNextEl(node)} className={styles.itemArrows}>
-            <ArrowSwiper className={`${styles.arrows} ${styles.arrowNext}`} />
-          </button>
-        </div>
-        <div className={styles.wrapperButon}>
+        <div className={styles.wrapperButton}>
           <button className={styles.button}>View collection</button>
         </div>
       </CoreSwiper>
