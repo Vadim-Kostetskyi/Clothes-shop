0cdfeede14155185c84e732030227f3ae5ae75fe
Tetiana Nosenko <tanya2016english@gmail.com>
2023-11-29 13:42:11 -0500

    create category items

--- src/components/ProductsCards/index.tsx
+++ src/components/ProductsCards/index.tsx
@@ -1,12 +1,13 @@
-import ProductCard from 'components/ProductCard';
 import React from 'react';
+import ProductCard from 'components/ProductCard';
 import { useGetProductsWithImagesQuery } from 'redux/productsApi';
+import styles from './index.module.scss';
 
 const Productscards = () => {
   const { data } = useGetProductsWithImagesQuery({ page: 0, size: 9 });
-  console.log(data);
+
   return (
-    <div>
+    <div className={styles.cardsWrapper}>
       {data?.products.map(({ id, title, price, size }) => {
         const images = data.images.find(item => item.id === id)?.images ?? [];
         return (
