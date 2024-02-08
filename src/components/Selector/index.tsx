7119113ded41ce02780d78b14d70ca9f8bc15f19
olena.mashurova <olena.mashurova@gmail.com>
2024-02-08 15:05:25 +0200

    feature(OS-127): fe create modal language window mob

--- src/components/Selector/index.tsx
+++ src/components/Selector/index.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import Select, { SingleValue, ActionMeta } from 'react-select';
+import React, { FC, CSSProperties } from 'react';
+import Select, { SingleValue, ActionMeta, StylesConfig } from 'react-select';
 import styles from './index.module.scss';
 
 export interface SelectOption {
@@ -14,12 +14,47 @@ export interface CountrySelectorProps {
     newValue: SingleValue<SelectOption>,
     actionMeta: ActionMeta<SelectOption>,
   ) => void;
+  isMobile?: boolean;
 }
 
-const Selector: FC<CountrySelectorProps> = ({ options, value, onChange }) => (
-  <div className={styles.wrapper}>
-    <Select options={options} value={value} onChange={onChange} />
-  </div>
-);
+const Selector: FC<CountrySelectorProps> = ({
+  options,
+  value,
+  onChange,
+  isMobile,
+}) => {
+  const customControlStyles: CSSProperties = {
+    cursor: 'pointer',
+    borderColor: '#959595',
+  };
+
+  const customMenuListStyles: CSSProperties = {
+    maxHeight: isMobile ? '225px' : '300px',
+  };
+
+  type IsMulti = false;
+
+  const selectStyle: StylesConfig<SelectOption, IsMulti> = {
+    control: provided => ({
+      ...provided,
+      ...customControlStyles,
+    }),
+    menuList: provided => ({
+      ...provided,
+      ...customMenuListStyles,
+    }),
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <Select
+        options={options}
+        value={value}
+        onChange={onChange}
+        styles={selectStyle}
+      />
+    </div>
+  );
+};
 
 export default Selector;
