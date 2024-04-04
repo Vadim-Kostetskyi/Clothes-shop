e0c641ca35c3c327203204873a393a782ccf8d32
Vadim <113288447+Vadim-Kostetskyi@users.noreply.github.com>
2024-04-04 17:04:02 +0300

    OS-186   [FE] Refactoring | Use valid HTML for Header and Footer menus (#76)
    * refactoring
    
    * refactor mobile
    
    * merging mobile and desktop header menu
    
    * codeFix

--- src/modules/core/containers/Header/index.tsx
+++ src/modules/core/containers/Header/index.tsx
@@ -2,7 +2,6 @@ import React, { useCallback, useState } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import CatalogMenu from 'components/CatalogMenu';
-import HamburgerMenu from 'modules/core/components/HamburgerMenu';
 import TopBar from 'modules/core/components/TopBar';
 import Input from 'modules/core/components/Input';
 import logo from 'assets/images/logo.png';
@@ -59,7 +58,7 @@ const Header = (): JSX.Element => {
             </div>
           </div>
           <div className={styles.wrapperMenuMobile}>
-            {isMenuOpen ? <HamburgerMenu /> : null}
+            {isMenuOpen ? <CatalogMenu /> : null}
             {isMenuOpen ? null : (
               <div className={styles.wrapperInput}>
                 <Input
