b1d3644e5073ce65b3ddf25ceee345396dbcf191
Vadim <113288447+Vadim-Kostetskyi@users.noreply.github.com>
2024-06-24 21:42:55 +0300

    OS-95   [FE] Create Shopping bag (mob) (#84)
    * adding mobile styles
    
    * deleted Fragment

--- src/modules/checkout/containers/ShoppingCartModal/index.tsx
+++ src/modules/checkout/containers/ShoppingCartModal/index.tsx
@@ -1,13 +1,12 @@
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useAppSelector, useGetViewportWidth } from 'hooks';
+import { useAppSelector } from 'hooks';
 import {
   memoizedSelectUniqueItems,
   selectOrderTotalPrice,
 } from 'redux/slices/shopping-cart';
 import ShoppingCart from 'modules/checkout/components/ShoppingCart';
 import OrderSummary from 'modules/checkout/components/OrderSummary';
-import { ViewportWidth } from 'utils/constants';
 
 type Properties = {
   onClose?: () => void;
@@ -21,25 +20,20 @@ const ShoppingCartModal: FC<Properties> = ({
   const { t } = useTranslation();
   const products = useAppSelector(memoizedSelectUniqueItems);
   const totalPrice = useAppSelector(selectOrderTotalPrice);
-  const isMobileWindowWidth = useGetViewportWidth(ViewportWidth.TABLET);
 
   return (
-    <>
-      {isMobileWindowWidth ? null : (
-        <ShoppingCart
-          title={isOrder ? t('order.summaryTitle') : t('shoppingCart.title')}
-          onClose={onClose}
-          products={products}
-          isOrder={isOrder}
-        >
-          <OrderSummary
-            totalPrice={totalPrice}
-            isOrder={isOrder}
-            amountProducts={products?.length}
-          />
-        </ShoppingCart>
-      )}
-    </>
+    <ShoppingCart
+      title={isOrder ? t('order.summaryTitle') : t('shoppingCart.title')}
+      onClose={onClose}
+      products={products}
+      isOrder={isOrder}
+    >
+      <OrderSummary
+        totalPrice={totalPrice}
+        isOrder={isOrder}
+        amountProducts={products?.length}
+      />
+    </ShoppingCart>
   );
 };
 
